2d2beb884bb1465e7627de91c161916b771bb73b
Fernando Lira <x00182229190@tcu.gov.br>
2017-02-21 17:23:06 -0300

    Added a screen for non-logged users and changed homepage to get categories

--- lihof/index.php
+++ lihof/index.php
@@ -10,44 +10,47 @@
  *
  * @package Adirondack
  */
-
-get_header(); ?>
-
-<?php
-	if ( is_front_page() && adirondack_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php adirondack_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+if (current_user_can("edit_post")) : /*  If the user is logged */ ?>
+	<?php get_header(); ?>
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<?php if (has_category()) : ?>
+					<?php $categories = get_categories( array(
+						'orderby' => 'name',
+						'parent'  => 0));?>
+					<?php foreach ( $categories as $category ) : ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background: url(<?php echo esc_attr( $url[0] ); ?>) no-repeat center; background-size: cover;">
+							<a href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>" rel="bookmark" class="entry-link">
+								<header class="entry-header">
+									<h1 class="entry-title"><?php echo esc_html($category->name); ?></h1>
+								</header><!-- .entry-header -->
+								<div class="link-button"><span>View more</span></div>
+							</a>
+						</article>
+					<?php endforeach;?> 
+				<?php else : ?>
+					<?php get_template_part( 'content', 'none' ); ?>
+				<?php endif; ?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+	<?php get_footer(); ?>
+	
+<?php else : /* If the user isn't logged */ ?>
+	<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<style>.cc-alert{left:3em;position:absolute;top:25vh;}</style>
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<div class="cc-alert">
+			<h1>Sorry!</h1>
+			<div>This is a closed community. Want to know more? Click here. Or 
+			<a href="<?php echo esc_url( wp_login_url( home_url() ) ); ?>">click here</a> if you want to login</div>
+		</div>
+	</body>
+<?php endif; ?>
